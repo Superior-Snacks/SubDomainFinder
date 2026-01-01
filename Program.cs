5e57ff16c6f227d7d98efe9f946a6065478b1bca
Superior-Snacks <joningiwork@gmail.com>
2026-01-01 17:48:45 +0000

    s

--- Program.cs
+++ Program.cs
@@ -15,26 +15,5 @@ namespace subDomainFinder
                 Console.WriteLine(item);
             }
         }
-        public async Task ResolveHostnameAsync(string domain)
-        {
-            try
-            {
-                // Get the host entry for the domain
-                IPHostEntry entry = await Dns.GetHostEntryAsync(domain);
-
-                Console.WriteLine($"Host Name: {entry.HostName}");
-
-                foreach (IPAddress ip in entry.AddressList)
-                {
-                    // You can filter by AddressFamily to separate IPv4 and IPv6
-                    string type = ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork ? "IPv4" : "IPv6";
-                    Console.WriteLine($"{type}: {ip}");
-                }
-            }
-            catch (System.Net.Sockets.SocketException e)
-            {
-                Console.WriteLine($"DNS Error: {e.Message}"); // e.g., Host not found
-            }
-        }
     }
 }
