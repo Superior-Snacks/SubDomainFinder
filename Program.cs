cf6209a5813bd9d31e2801ec73009c66988e5dde
Superior-Snacks <joningiwork@gmail.com>
2026-01-02 23:49:06 +0000

    s

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace subDomainFinder
             IPAddress[] ips = await Dns.GetHostAddressesAsync(domain);
             foreach (var item in ips)
             {
+                Thread.Sleep(400);
                 Console.WriteLine(item);
             }
         }
