d83104ea3786f4d4cd9bdcf5ec58a8f40813a675
Superior-Snacks <joningiwork@gmail.com>
2026-02-09 12:13:46 +0000

    s

--- Program.cs
+++ Program.cs
@@ -9,15 +9,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Text.Json;
-
+//string baseIp = "89.104.145.151";
 
 namespace subDomainFinder
 {
 class Program
     {
-        static async Task Main()
+        static async Task Main(string[] args)
         {
-            string baseIp = "89.104.145.151";
+            if (args.Length != 1)
+            {
+                Console.WriteLine("usage subDomainFinder.exe domains");
+            }
         }
     }
 }
