e603604b379ec6cfef5a91944d0f7c865a0be0dc
Superior-Snacks <joningiwork@gmail.com>
2026-02-10 18:57:56 +0000

    s

--- Database.cs
+++ Database.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
 using System.Linq;
-using Dapper;
+using static System.Net.WebRequestMethods;
 
 namespace subDomainFinder
 {
-    internal class Database
+    public static class Database
     {
+        private static string DbFile = "recon.db";
+        private static string ConnectionString = $"Data Source={DbFile};Version=3;";
+
+        public static void Initialize()
+        {
+            if (!File.Exists(DbFile))
+            {
+                SQLiteConnection.CreateFile(DbFile);
+            }
+
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+                // We create a table with a UNIQUE constraint on the Subdomain column
+                // This prevents duplicates automatically!
+                string sql = @"
+                CREATE TABLE IF NOT EXISTS Subdomains (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    RootDomain TEXT NOT NULL,
+                    Subdomain TEXT NOT NULL UNIQUE,
+                    FirstSeen DATE DEFAULT CURRENT_TIMESTAMP
+                );";
+
+                connection.Execute(sql);
+            }
+        }
     }
 }
