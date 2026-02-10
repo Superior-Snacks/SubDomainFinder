fd4cfebacfc4db648a7a974fcc25f5f92a081460
Superior-Snacks <joningiwork@gmail.com>
2026-02-10 18:51:51 +0000

    s

---
+++ Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using Dapper;
+
+namespace subDomainFinder
+{
+    internal class Database
+    {
+    }
+}
